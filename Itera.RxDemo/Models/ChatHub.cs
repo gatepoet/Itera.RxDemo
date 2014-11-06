a277fa9f0bd2a2ce54c9f4da48163e655b670c76
gatepoet <kristoffer.deinoff@gmail.com>
2014-11-06 01:00:44 +0100

    Added chat

--- Itera.RxDemo/Models/ChatHub.cs
+++ Itera.RxDemo/Models/ChatHub.cs
@@ -5,6 +5,10 @@ using System.Linq;
 using System.Reactive.Subjects;
 using System.Web;
 using SignalR.Reactive;
+using System.Reactive.Linq;
+using System.Reactive;
+using System.Collections.Specialized;
+using System.Collections.ObjectModel;
 
 namespace Itera.RxDemo.Models
 {
@@ -13,33 +17,71 @@ namespace Itera.RxDemo.Models
         public string From { get; set; }
         public string Text { get; set; }
     }
+    public class User
+    {
+        public string ClientId { get; set; }
+        public string Name { get; set; }
+    }
     public class ChatHub : Hub
     {
         private static Dictionary<string, Subject<Message>> rooms = new Dictionary<string, Subject<Message>>();
-        private static Dictionary<string, Dictionary<string, string>> users = new Dictionary<string, Dictionary<string, string>>();
+        private static Dictionary<string, ObservableCollection<User>> users = new Dictionary<string, ObservableCollection<User>>();
+
         public void Join(string room, string name)
         {
             if (!rooms.ContainsKey(room))
             {
                 var obs = new Subject<Message>();
-                obs.ToClientside().Observable<ChatHub>("Message");
+                obs.ToClientside().Observable<ChatHub>(room + "-messages");
                 rooms.Add(room, obs);
             }
             if (!users.ContainsKey(room))
             {
-                users.Add(room, new Dictionary<string,string>());
+                var coll = new ObservableCollection<User>();
+                Observable.FromEventPattern<NotifyCollectionChangedEventHandler,NotifyCollectionChangedEventArgs>(
+                    c => coll.CollectionChanged += c,
+                    c => coll.CollectionChanged -= c)
+
+                    .Select(ch => coll.Select(u => u.Name).ToArray())
+                    .ToClientside().Observable<ChatHub>(room + "-users");
+                users.Add(room, coll);
             }
-            if (!users[room].ContainsKey(Context.ConnectionId))
+            var user = users[room].SingleOrDefault(u => u.ClientId == Context.ConnectionId);
+            if (user != null)
             {
-                users[room].Add(Context.ConnectionId, name);
+                users[room].Remove(user);
             }
+            users[room].Add(new User
+            {
+                ClientId = Context.ConnectionId,
+                Name = name
+            });
         }
         public void Send(string room, string text)
         {
             rooms[room].OnNext(new Message{
-                From = users[room][Context.ConnectionId],
+                From = users[room].Single(u => u.ClientId == Context.ConnectionId).Name,
                 Text = text
             });
+        }
+    }
+    public static class ObservableCollectionExtensions
+    {
+        public static IObservable<NotifyCollectionChangedEventArgs>
+            GetObservableChanges<T>(this ObservableCollection<T> collection)
+        {
+            return Observable.FromEvent<
+                NotifyCollectionChangedEventHandler, NotifyCollectionChangedEventArgs>(
+                    h => collection.CollectionChanged += h,
+                    h => collection.CollectionChanged -= h);
+        }
+
+        public static IObservable<T> GetObservableAddedValues<T>(
+            this ObservableCollection<T> collection)
+        {
+            return collection.GetObservableChanges()
+                .Where(evnt => evnt.Action == NotifyCollectionChangedAction.Add)
+                .SelectMany(evnt => evnt.NewItems.Cast<T>());
         }
     }
 }
---
+++ Itera.RxDemo/Scripts/chat-example.js
@@ -0,0 +1,44 @@
+﻿$(function () {
+    var myHub = $.connection.chatHub;
+
+    $.connection.hub.start();
+    var subscription;
+    $("#join-chat-room").click(function () {
+        var room = $("#room").val();
+
+        if (subscription)
+            subscription.isStopped = true;
+
+        subscription = myHub.server
+           .observe(room + "-messages")
+           .subscribe(function (message) {
+               console.log(message);
+               $("#chat-section").append("<span style='font-weight: bold'>" + message.From + "</span> says:<br /><span>" + message.Text + "</span><br />")
+           });
+
+        $("#room-name").text(room);
+
+        myHub.server
+           .observe(room + "-users")
+           .subscribe(function (message) {
+               $("#participants").text(message);
+           });
+
+        myHub.server.join(
+            room,
+            $("#username").val());
+
+    });
+    $("#chat-input").keydownAsObservable()
+        .where(function (e) {
+            return e.keyCode === 13;
+        })
+        .subscribe(function () { $("#send-message").click(); });
+
+    $("#send-message").click(function () {
+        myHub.server.send(
+            $("#room").val(),
+            $("#chat-input").val());
+    });
+
+});
